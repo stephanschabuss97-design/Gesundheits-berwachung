9c1c9a83500c8da76f8c0f5696f874c4f5276941
Stephan Schabuß <stephan.schabuss@aon.at>
2025-11-20 09:25:30 +0100

    Layout update

--- app/modules/hub/index.js
+++ app/modules/hub/index.js
@@ -57,7 +57,7 @@
     if (!orbit || !buttons.length) return;
 
     const getBaseFactor = () =>
-      global.matchMedia('(max-width: 640px)').matches ? 0.5 : 0.45;
+      global.matchMedia('(max-width: 640px)').matches ? 0.55 : 0.5;
 
     const setPositions = () => {
       const rect = orbit.getBoundingClientRect();
